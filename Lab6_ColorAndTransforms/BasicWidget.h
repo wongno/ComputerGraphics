6f1ba0bca8552e1c46800b22bc6d168f895c35f6
Noelle Wong <wong.no@husky.neu.edu>
2020-04-15 17:57:01 -0400

    lab 6

--- Lab6_ColorAndTransforms/BasicWidget.h
+++ Lab6_ColorAndTransforms/BasicWidget.h
@@ -9,39 +9,39 @@
  */
 class BasicWidget : public QOpenGLWidget, protected QOpenGLFunctions
 {
-  Q_OBJECT
+    Q_OBJECT
 
 private:
-  QString vertexShaderString() const;
-  QString fragmentShaderString() const;
-  void createShader();
-  QOpenGLVertexArrayObject vao_;
+    QString vertexShaderString() const;
+    QString fragmentShaderString() const;
+    void createShader();
+    QOpenGLVertexArrayObject vao_;
 
-  QMatrix4x4 model_;
-  QMatrix4x4 view_;
-  QMatrix4x4 projection_;
-  
+    QMatrix4x4 model_;
+    QMatrix4x4 view_;
+    QMatrix4x4 projection_;
+
 protected:
-  // Required interaction overrides
-  void keyReleaseEvent(QKeyEvent* keyEvent) override;
+    // Required interaction overrides
+    void keyReleaseEvent(QKeyEvent* keyEvent) override;
 
-  // Required overrides form QOpenGLWidget
-  void initializeGL() override;
-  void resizeGL(int w, int h) override;
-  void paintGL() override;
+    // Required overrides form QOpenGLWidget
+    void initializeGL() override;
+    void resizeGL(int w, int h) override;
+    void paintGL() override;
 
-  QOpenGLBuffer vbo_;
-  QOpenGLBuffer ibo_;
-  // TODO:  Remove the cbo_!
-  QOpenGLBuffer cbo_;
-  // END TODO
-  QOpenGLShaderProgram shaderProgram_;
-  QOpenGLDebugLogger logger_;
-  
+    QOpenGLBuffer vbo_;
+    QOpenGLBuffer ibo_;
+    // TODO:  Remove the cbo_!
+    // QOpenGLBuffer cbo_;
+    // END TODO
+    QOpenGLShaderProgram shaderProgram_;
+    QOpenGLDebugLogger logger_;
+
 public:
-  BasicWidget(QWidget* parent=nullptr);
-  virtual ~BasicWidget();
-  
-  // Make sure we have some size that makes sense.
-  QSize sizeHint() const {return QSize(800,600);}
+    BasicWidget(QWidget* parent = nullptr);
+    virtual ~BasicWidget();
+
+    // Make sure we have some size that makes sense.
+    QSize sizeHint() const { return QSize(800, 600); }
 };
